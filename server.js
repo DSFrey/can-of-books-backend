87d66315ee724d7036f291a81036912370783e13
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-08 16:48:59 -0500

    Create schema and connect to database

--- server.js
+++ server.js
@@ -3,16 +3,40 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
-app.use(cors());
-
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+app.use(cors());
+mongoose.connect(process.env.MONGO_URL);
 
-  response.send('test request received')
+const Book = require('./models/book.js');
 
-})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+
+app.get('/books', getBooks);
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not available');
+});
+
+async function getBooks(request, response, next) {
+  try {
+    let results = await Book.find();
+    response.status(200).send(results);
+  } catch (error) {
+    next(error);
+  }
+}
+
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
